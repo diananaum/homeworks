309d4ff4e8204e69c34f8a0c3e5d5a9d487249e9
Diana Vakhitova <dianaum0809@gmail.com>
2023-02-02 15:34:18 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function solveEquation(a, b, c) {
+    let arr = [];
+    let d = Math.pow(b,2) - 4 * a * c;
+
+    if (d === 0) {
+        const x = -b / (2*a);
+        arr.push(x);
+        // return arr = new Array(1)
+    } else if (d > 0) {
+        const x1 = -b + Math.sqrt(d) / (2 * a);
+        const x2 = -b - Math.sqrt(d) / (2 * a);
+        arr.push(x1, x2);
+    }
+
+    return arr;
+  }  
